589ce1e0d5b3999b317bfd2db4679c8ddc21d972
mohsen.abrishami <mohsen.abrishami@partsoftware.com>
2023-01-31 11:59:17 +0330

    Initial vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,9 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="w-10/12 h-32 mx-auto mt-24 bg-white flex justify-center p-2">
+    <div class="h-28 w-60 bg-red-200 ml-5 p-2 inline-block">1</div>
+    <div class="h-28 w-60 bg-blue-200 ml-5 p-2 inline-block">2</div>
+    <div class="h-28 w-60 bg-yellow-200 ml-5 p-2 inline-block">3</div>
+    <div class="h-28 w-60 bg-orange-200 ml-5 p-2 inline-block">4</div>
+    <div class="h-28 w-60 bg-amber-200 ml-5 p-2 inline-block">5</div>
+  </div>
 </template>
