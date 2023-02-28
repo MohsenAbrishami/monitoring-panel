eb20f631cfa9a10ffa2749de778d700d106a92f4
mohsen.abrishami <mohsen.abrishami@partsoftware.com>
2023-02-28 17:10:19 +0330

    Add other chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,43 +1,3 @@
-<script setup>
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-} from 'chart.js'
-import { Line } from 'vue-chartjs'
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-)
-
-const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-        {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 39, 10, 40, 39, 80, 40],
-        },
-    ],
-}
-
-const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-}
-</script>
-
 <template>
     <div class="w-9/12 bg-white my-5 mx-auto p-5 rounded-lg">
         <div class="w-2/12 inline-block float-left pt-10">
@@ -62,3 +22,89 @@ const options = {
         </div>
     </div>
 </template>
+
+<script setup>
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+import { onMounted, reactive, ref } from 'vue'
+import axios from 'axios'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+const cpu = ref([40, 39, 10, 40, 39, 80, 40])
+const memory = ref([10, 2, 40, 50, 30, 70, 40])
+const hardDisk = ref([1, 20, 4, 5, 30, 7, 40])
+const webServer = ref([5, 10, 30, 40, 20, 10, 40])
+const network = ref([80, 2, 40, 50, 30, 7, 40])
+
+const data = ref({
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'CPU',
+            backgroundColor: '#60a5fa',
+            data: cpu,
+        },
+        {
+            label: 'Memory',
+            backgroundColor: '#4ade80',
+            data: memory,
+        },
+        {
+            label: 'hdd',
+            backgroundColor: '#f87171',
+            data: hardDisk,
+        },
+        {
+            label: 'web_server',
+            backgroundColor: '#34d399',
+            data: webServer,
+        },
+        {
+            label: 'network',
+            backgroundColor: '#c084fc',
+            data: network,
+        },
+    ],
+})
+
+const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+}
+
+const resourceLog = reactive({
+    history: null,
+})
+
+function getResourceLogHistories() {
+    axios.get('http://localhost:8000/monitor/history/2020-01-01/2023-12-12')
+        .then((value) => {
+            resourceLog.history = value.data
+            resourceLog.history[0].forEach((element) => {
+                console.info(element.id)
+            })
+        })
+}
+
+onMounted(() => {
+    getResourceLogHistories()
+})
+</script>
