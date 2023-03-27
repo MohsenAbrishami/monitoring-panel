b04fcf01377305cd91b30a63f02e24a37b698474
mohsen.abrishami <mohsen.abrishami@partsoftware.com>
2023-03-27 11:58:25 +0430

    Filter charts by user

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,36 +1,46 @@
 <template>
     <div class="w-9/12 bg-white my-5 mx-auto p-5 rounded-lg">
-        <div class="w-2/12 inline-block float-left pt-10">
-            <button
-                class="w-32 bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                @click="generateChart()"
-            >
-                CPU
-            </button>
-            <button
-                class="w-32 bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-3"
-                @click="generateChart()"
-            >
-                Memory
-            </button>
-            <button
-                class="w-32 bg-red-400 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-3"
-                @click="generateChart()"
-            >
-                HDD
-            </button>
-            <button
-                class="w-32 bg-emerald-400 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded mt-3"
-                @click="generateChart()"
-            >
-                Web Server
-            </button>
-            <button
-                class="w-32 bg-purple-400 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mt-3"
-                @click="generateChart()"
-            >
-                Network
-            </button>
+        <div class="w-2/12 inline-block float-left pt-16">
+            <div class="block pt-2">
+                <input
+                    v-model="resources.cpu"
+                    type="checkbox"
+                    @click="generateChart('cpu')"
+                >
+                <span class="text-blue-500 font-bold mx-4">CPU</span>
+            </div>
+            <div class="block pt-2">
+                <input
+                    v-model="resources.memory"
+                    type="checkbox"
+                    @click="generateChart('memory')"
+                >
+                <span class="text-green-500 font-bold mx-4">Memory</span>
+            </div>
+            <div class="block pt-2">
+                <input
+                    v-model="resources.hardDisk"
+                    type="checkbox"
+                    @click="generateChart('hardDisk')"
+                >
+                <span class="text-red-500 font-bold mx-4">HDD</span>
+            </div>
+            <div class="block pt-2">
+                <input
+                    v-model="resources.webServer"
+                    type="checkbox"
+                    @click="generateChart('webServer')"
+                >
+                <span class="text-emerald-400 font-bold mx-4">Web Server</span>
+            </div>
+            <div class="block pt-2">
+                <input
+                    v-model="resources.network"
+                    type="checkbox"
+                    @click="generateChart('network')"
+                >
+                <span class="text-purple-400 font-bold mx-4">Network</span>
+            </div>
         </div>
         <div class="w-10/12 inline-block h-80">
             <Line :data="data" :options="options" />
@@ -74,6 +84,14 @@ const resourceLog = reactive({
     history: null,
 })
 
+const resources = reactive({
+    cpu: true,
+    memory: true,
+    hardDisk: true,
+    webServer: true,
+    network: true,
+})
+
 function getResourceLogHistories() {
     axios.get('http://localhost:8000/monitor/history/2023-03-14/2023-03-26')
         .then((value) => {
@@ -82,36 +100,52 @@ function getResourceLogHistories() {
         })
 }
 
-function generateChart() {
+function generateChart(key) {
+    resources[key] = !resources[key]
+
     data.value = {
         labels: resourceLog.history?.labels,
-        datasets: [
-            {
-                label: 'CPU',
-                backgroundColor: '#60a5fa',
-                data: resourceLog.history?.resource_log_count.cpu,
-            },
-            {
-                label: 'Memory',
-                backgroundColor: '#4ade80',
-                data: resourceLog.history?.resource_log_count.memory,
-            },
-            {
-                label: 'hdd',
-                backgroundColor: '#f87171',
-                data: resourceLog.history?.resource_log_count.hard_disk,
-            },
-            {
-                label: 'web_server',
-                backgroundColor: '#34d399',
-                data: resourceLog.history?.resource_log_count.network,
-            },
-            {
-                label: 'network',
-                backgroundColor: '#c084fc',
-                data: resourceLog.history?.resource_log_count.web_server,
-            },
-        ],
+        datasets: [],
+    }
+
+    if (resources.cpu) {
+        data.value.datasets.push({
+            label: 'CPU',
+            backgroundColor: '#60a5fa',
+            data: resourceLog.history?.resource_log_count.cpu,
+        })
+    }
+
+    if (resources.memory) {
+        data.value.datasets.push({
+            label: 'Memory',
+            backgroundColor: '#4ade80',
+            data: resourceLog.history?.resource_log_count.memory,
+        })
+    }
+
+    if (resources.hardDisk) {
+        data.value.datasets.push({
+            label: 'hdd',
+            backgroundColor: '#f87171',
+            data: resourceLog.history?.resource_log_count.hard_disk,
+        })
+    }
+
+    if (resources.webServer) {
+        data.value.datasets.push({
+            label: 'web_server',
+            backgroundColor: '#34d399',
+            data: resourceLog.history?.resource_log_count.network,
+        })
+    }
+
+    if (resources.network) {
+        data.value.datasets.push({
+            label: 'network',
+            backgroundColor: '#c084fc',
+            data: resourceLog.history?.resource_log_count.web_server,
+        })
     }
 }
 
