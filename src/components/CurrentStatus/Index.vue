af34f12a94e12340b7522a1a1d2b60bc821e1a96
mohsen.abrishami <mohsenabrishami.ma@gmail.com>
2023-11-11 17:24:25 +0330

    Add loading to chart part

--- src/components/CurrentStatus/Index.vue
+++ src/components/CurrentStatus/Index.vue
@@ -6,7 +6,9 @@
             class="w-10 inline-block float-right mr-3 cursor-pointer"
             @click.prevent="getCurrentStatus()"
         >
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
+
+        <div v-if="isLoading" class="loader" />
+        <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
             <StatusItem
                 title="Cpu Usage"
                 :result="statuses.memory"
@@ -42,7 +44,9 @@
 </template>
 
 <script setup>
-import { onBeforeUnmount, onMounted, reactive } from 'vue'
+import {
+    onBeforeUnmount, onMounted, reactive, ref
+} from 'vue'
 import axios from 'axios'
 import StatusItem from './StatusItem.vue'
 
@@ -53,6 +57,8 @@ const statuses = reactive({
     network: 'Loading..',
     webServer: 'Loading..',
 })
+
+const isLoading = ref(false)
 
 onMounted(() => {
     getCurrentStatus()
@@ -67,6 +73,7 @@ const statusChecker = setInterval(() => {
 }, 180000)
 
 function getCurrentStatus() {
+    isLoading.value = true
     axios.get('monitor/current')
         .then((value) => {
             statuses.cpu = `${Number(value.data.cpu).toFixed(2)} %`
@@ -74,6 +81,34 @@ function getCurrentStatus() {
             statuses.memory = `${Number(value.data.memory).toFixed(2)} %`
             statuses.network = value.data.network
             statuses.webServer = value.data.web_server
+            isLoading.value = false
         })
 }
 </script>
+
+<style scoped>
+.loader {
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
